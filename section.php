1fe026c68295a830c7426a1c6c21e3905d25114d
Aleksander Hansson <aleksander.hansson@me.com>
2013-04-17 12:55:04 +0200

    1.2

--- section.php
+++ section.php
@@ -4,7 +4,7 @@
 	Author: Aleksander Hansson
 	Author URI: http://ahansson.com
 	Demo: http://squared.ahansson.com
-	Version: 1.1
+	Version: 1.2
 	Description: Squared is an interactive grid system that supports up to 15 images or icons with your custom text. It is awesome!
 	Class Name: Squared
 	Workswith: main, templates
